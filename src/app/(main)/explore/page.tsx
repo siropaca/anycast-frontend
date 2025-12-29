16f4dfbfcbfd8c6492b02913aad9433acd48d382
siropaca <ryhshr3@gmail.com>
2025-12-29 22:03:58 +0900

    feat: 探索ページで検索クエリを表示

--- src/app/(main)/explore/page.tsx
+++ src/app/(main)/explore/page.tsx
@@ -1,10 +1,22 @@
 import type { Metadata } from 'next';
 import { Pages } from '@/libs/pages';
+import type { ExploreSearchParams } from '@/libs/pages/mainPages';
 
 export const metadata: Metadata = {
   title: Pages.explore.title,
 };
 
-export default function ExplorePage() {
-  return <div>Explore</div>;
+interface Props {
+  searchParams: Promise<ExploreSearchParams>;
+}
+
+export default async function ExplorePage({ searchParams }: Props) {
+  const { q } = await searchParams;
+
+  return (
+    <div>
+      <div>Explore</div>
+      <div>検索クエリ: {q}</div>
+    </div>
+  );
 }
