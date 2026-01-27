bff40860924757e8e744461e3d5d59c057ae0d41
siropaca <ryhshr3@gmail.com>
2026-01-27 13:56:19 +0900

    feat: HeaderSearchInputにInputコンポーネントを適用しクリア機能を追加

--- src/components/navigation/Header/HeaderSearchInput.tsx
+++ src/components/navigation/Header/HeaderSearchInput.tsx
@@ -1,33 +1,42 @@
 'use client';
 
+import { MagnifyingGlassIcon } from '@phosphor-icons/react';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { Input } from '@/components/inputs/Input/Input';
 import { Pages } from '@/libs/pages';
 
-// TODO: 仮コンポーネント
 export function HeaderSearchInput() {
   const router = useRouter();
 
   const [value, setValue] = useState('');
 
-  const handleFocus = () => {
+  function handleFocus() {
     router.push(Pages.explore.path({ q: value || undefined }));
-  };
+  }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const newValue = e.target.value;
     setValue(newValue);
     router.push(Pages.explore.path({ q: newValue || undefined }));
-  };
+  }
+
+  function handleClear() {
+    setValue('');
+    router.push(Pages.explore.path());
+  }
 
   return (
-    <input
+    <Input
       type="text"
-      placeholder="検索"
-      className="border"
+      placeholder="何を聴きたいですか？"
       value={value}
+      leftIcon={<MagnifyingGlassIcon />}
+      clearable
+      className="w-80"
       onFocus={handleFocus}
       onChange={handleChange}
+      onClear={handleClear}
     />
   );
 }
